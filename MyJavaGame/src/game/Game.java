20d5d316f2dbcc4a40641aaed1b5b48982a05fbf
alvin <alvin@10.64.202.191>
2018-09-06 16:58:42 -0400

    Refactored Game.java for organization

--- MyJavaGame/src/game/Game.java
+++ MyJavaGame/src/game/Game.java
@@ -6,26 +6,20 @@ import java.awt.RenderingHints;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/*
+ * Name: Alvin Quijano 
+ * Version: 1.0 
+ * Description: This is the main game class, it creates the game window and creates
+ * the ball object and moves it around the window
+ */
+
+
 public class Game extends JPanel {
 
-	// ball coordinates 
-	int x = 0;
-	int y = 0;
-	int xa = 1;
-	int ya = 1;
+	Ball ball = new Ball(this);
 	
-	private void moveBall() {
-		if (x + xa < 0)
-			xa = 1;
-		if (x + xa > getWidth() - 30)
-			xa = -1;
-		if (y + ya < 0)
-			ya = 1;
-		if (y + ya > getHeight() - 30)
-			ya = -1;
-		
-		x = x + xa;
-		y = y + ya;
+	private void move() {
+		ball.move();
 	}
 	
 	// method that paints the ball to the canvas 
@@ -35,7 +29,7 @@ public class Game extends JPanel {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.fillOval(x, y, 30, 30);
+		ball.paint(g2d);
 	}
 
 
@@ -50,7 +44,7 @@ public class Game extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		while (true) {
-			game.moveBall();
+			game.move();
 			game.repaint();
 			Thread.sleep(10);
 		}

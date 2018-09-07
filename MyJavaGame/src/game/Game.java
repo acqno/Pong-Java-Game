b894a05b5d9fe68b462297a07182f4ba169b0f0e
alvin <alvin@10.64.202.191>
2018-09-07 10:17:01 -0400

    Instantiated racquet object

--- MyJavaGame/src/game/Game.java
+++ MyJavaGame/src/game/Game.java
@@ -3,12 +3,14 @@ package game;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /*
  * Name: Alvin Quijano 
- * Version: 1.0 
+ * Version: 1.2
  * Description: This is the main game class, it creates the game window and creates
  * the ball object and moves it around the window
  */
@@ -17,9 +19,30 @@ import javax.swing.JPanel;
 public class Game extends JPanel {
 
 	Ball ball = new Ball(this);
+	Racquet racquet = new Racquet(this);
+	
+	public Game() {
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				racquet.keyPressed(e);
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				racquet.keyReleased(e);
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {}
+		});
+		setFocusable(true);
+	}
 	
 	private void move() {
 		ball.move();
+		racquet.move();
 	}
 	
 	// method that paints the ball to the canvas 

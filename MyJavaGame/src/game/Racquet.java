2d174b379b2ae3a6ec472abeb9c8ba925578f59c
alvin <alvin@10.64.202.191>
2018-09-07 10:06:00 -0400

    Added key events to move racquet

--- MyJavaGame/src/game/Racquet.java
+++ MyJavaGame/src/game/Racquet.java
@@ -1,9 +1,11 @@
 package game;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
 /*
  * Alvin Quijano 
- * Version: 0.2
+ * Version: 0.3
  * Description: This is the Racquet class file 
  */
 public class Racquet {
@@ -29,5 +31,21 @@ public class Racquet {
 	// This is the paint method, it draws the racquet sprite on the game board
 	public void paint(Graphics2D g) {
 		g.fillRect(x, 330, 60, 10);
+	}
+	
+	// This method stops the racquet from moving when user releases a button
+	public void keyReleased(KeyEvent e) {
+		xa = 0;
+	}
+	
+	// This method handles the user input and moves the racquet left or right
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT){
+			xa = -1;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			xa = 1;
+		}
+
 	}
 }

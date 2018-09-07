dba04bf5f1fd8a0c3763ba35cf3ab297ea17e987
alvin <alvin@10.64.202.191>
2018-09-07 09:53:33 -0400

    Added constructor, move and paint method

--- MyJavaGame/src/game/Racquet.java
+++ MyJavaGame/src/game/Racquet.java
@@ -1,11 +1,33 @@
 package game;
 
+import java.awt.Graphics2D;
 /*
  * Alvin Quijano 
- * Version: 0.1
+ * Version: 0.2
  * Description: This is the Racquet class file 
  */
 public class Racquet {
-
 	
+	// private instance variables 
+	int x = 0;
+	int xa = 0; 
+	private Game game; 
+	
+	// constructor
+	public Racquet(Game game) {
+		this.game = game;
+	}
+	
+	// public methods 
+	
+	// This is the move method, it moves the racque
+	public void move() {
+		if (x + xa > 0 && x + xa < game.getWidth() - 60)
+			x = x + xa; 
+	}
+	
+	// This is the paint method, it draws the racquet sprite on the game board
+	public void paint(Graphics2D g) {
+		g.fillRect(x, 330, 60, 10);
+	}
 }
